b09558703551a352c79489557bb0b1b775d7e9ca
DShulkin <thedavidshulkin@gmail.com>
2022-04-05 02:34:32 -0400

    scripts updated

--- js/app.js
+++ js/app.js
@@ -19,12 +19,10 @@ const data = {
     labels, 
     datasets: [{
         data:[750, 1200, 1000, 2000, 1500, 1750, 800, 1900, 2250, 1500, 2500],
-        label: 'traffic',
         fill: true,
         backgroundColor: ["rgba(116, 119, 191, 0.3)"],
         borderColor: ["rgba(116, 119, 191, 1)"],
         borderWidth: 1,
-    
     },
     ],
 }
@@ -38,3 +36,46 @@ const config = {
 }
 
 const lineChart = new Chart(traffic, config)
+
+
+
+
+
+
+// const labels = [
+//     '16-22',
+//     '23-29', 
+//     '30-5', 
+//     '6-12', 
+//     '13-19', 
+//     '20-26', 
+//     '27-3', 
+//     '4-10', 
+//     '11-17', 
+//     '18-24', 
+//     '25-31'
+//     ]
+
+// const data = {
+//     labels: labels,
+//     datasets: [{
+//     label: '',
+//     backgroundColor: 'rgb(255, 99, 132)',
+//     borderColor: 'rgb(255, 99, 132)',
+//             data: [750, 1200, 1000, 2000, 1500, 1750, 800, 1900, 2250, 1500, 2500],
+
+//     }]
+// };
+
+// const config = {
+//     type: 'line',
+//     data: data,
+//     options: {
+//       responsive: true
+//     }
+//   };
+
+//   const myChart = new Chart(
+//     document.getElementById('traffic-chart'),
+//     config
+//   );
