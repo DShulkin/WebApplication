2772a3c73fb916ea6fd2050617feb21dff484532
DShulkin <thedavidshulkin@gmail.com>
2022-04-05 14:01:43 -0400

    updated files

--- js/app.js
+++ js/app.js
@@ -15,11 +15,13 @@ const labels = [
 '25-31'
 ]
 
+
 const data = {
     labels, 
     datasets: [{
         data:[750, 1200, 1000, 2000, 1500, 1750, 800, 1900, 2250, 1500, 2500],
         fill: true,
+        label: '',
         backgroundColor: ["rgba(116, 119, 191, 0.3)"],
         borderColor: ["rgba(116, 119, 191, 1)"],
         borderWidth: 1,
@@ -29,53 +31,22 @@ const data = {
 
 const config = {
     type: 'line',
-    data: data,
+    data,
     options: {
         responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    boxWidth: 0
+                }
+            }
+        }
     }
 }
 
 const lineChart = new Chart(traffic, config)
-
-
-
-
-
-
-// const labels = [
-//     '16-22',
-//     '23-29', 
-//     '30-5', 
-//     '6-12', 
-//     '13-19', 
-//     '20-26', 
-//     '27-3', 
-//     '4-10', 
-//     '11-17', 
-//     '18-24', 
-//     '25-31'
-//     ]
-
-// const data = {
-//     labels: labels,
-//     datasets: [{
-//     label: '',
-//     backgroundColor: 'rgb(255, 99, 132)',
-//     borderColor: 'rgb(255, 99, 132)',
-//             data: [750, 1200, 1000, 2000, 1500, 1750, 800, 1900, 2250, 1500, 2500],
-
-//     }]
-// };
-
-// const config = {
-//     type: 'line',
-//     data: data,
-//     options: {
-//       responsive: true
-//     }
-//   };
-
-//   const myChart = new Chart(
-//     document.getElementById('traffic-chart'),
-//     config
-//   );
