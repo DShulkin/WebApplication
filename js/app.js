a5b3ffdf7e8ec209d266190bec3307e81e1a6779
DShulkin <thedavidshulkin@gmail.com>
2022-04-05 15:20:09 -0400

    updated

--- js/app.js
+++ js/app.js
@@ -1,37 +1,19 @@
-
-const traffic = document.querySelector('#traffic-chart').getContext('2d')
-
-const labels = [
-'16-22',
-'23-29', 
-'30-5', 
-'6-12', 
-'13-19', 
-'20-26', 
-'27-3', 
-'4-10', 
-'11-17', 
-'18-24', 
-'25-31'
-]
-
-
+const trafficCanvas = document.querySelector('#traffic-chart').getContext('2d')
 const data = {
-    labels, 
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
-        data:[750, 1200, 1000, 2000, 1500, 1750, 800, 1900, 2250, 1500, 2500],
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
         fill: true,
-        label: '',
-        backgroundColor: ["rgba(116, 119, 191, 0.3)"],
-        borderColor: ["rgba(116, 119, 191, 1)"],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderColor: ['rgba(116, 119, 191, 1)'],
         borderWidth: 1,
-    },
-    ],
-}
+    }]
+};
 
-const config = {
-    type: 'line',
+
+const trafficOptions = {
     data,
+    type: 'line',
     options: {
         responsive: true,
         scales: {
@@ -41,12 +23,16 @@ const config = {
         },
         plugins: {
             legend: {
-                labels: {
-                    boxWidth: 0
-                }
+                display: false
             }
         }
     }
 }
 
-const lineChart = new Chart(traffic, config)
+const lineChart = new Chart(trafficCanvas, trafficOptions)
+
+
+
+
+
+
