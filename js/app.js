7969998d2b2e628505f11c30c0dc789199f9a0e9
DShulkin <thedavidshulkin@gmail.com>
2022-04-05 22:48:07 -0400

    all three charts

--- js/app.js
+++ js/app.js
@@ -87,6 +87,43 @@ const barChart = new Chart(dailyCanvas, dailyOptions)
 
 /*------------------------ MOBILE USERS ---------------------------------*/
 
+const mobileCanvas = document.querySelector('#doughnut-chart').getContext('2d')
+
+const mobileData = {
+    labels: ['Desktop', 'Tablet', 'Mobile'],
+    datasets: [{
+        label: 'Mobile Users',
+        data: [300, 100, 100],
+        background: '#7477BF',
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+        borderWidth: 1,
+    }]
+}
+
+const mobileOptions = {
+    type: 'doughnut',
+    data: mobileData,
+    aspectRatio: 1.9,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'right',
+                labels: {
+                    boxWidth: 20,
+                    fontStyle: 'bold'
+                }
+            }
+        }
+    }
+}
+
+const mobileChart = new Chart(mobileCanvas, mobileOptions)
+
 /*------------------------------------------------------------------------*/
 
 
